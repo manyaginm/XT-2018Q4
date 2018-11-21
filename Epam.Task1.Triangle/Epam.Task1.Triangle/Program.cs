29bd3d8a221a5a2c45605db6d333021d18175da8
Mikhail Manyagin <manyaginm@gmail.com>
2018-11-21 23:46:49 +0400

    Epam.Task1.Triangle

---
+++ Epam.Task1.Triangle/Epam.Task1.Triangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Epam.Task1.Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < k; i++)
+            {
+                int z = 0;
+                while (z <= i)
+                {
+                    Console.Write("*");
+                    z++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
