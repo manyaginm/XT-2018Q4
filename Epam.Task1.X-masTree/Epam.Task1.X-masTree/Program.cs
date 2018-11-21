4cb11b5aef98917e35b0466aed0587e3d6741c5a
Mikhail Manyagin <manyaginm@gmail.com>
2018-11-21 23:57:35 +0400

    X-Mas Tree

---
+++ Epam.Task1.X-masTree/Epam.Task1.X-masTree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Epam.Task1.X_masTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = int.Parse(Console.ReadLine());
+            for (int k = 0; k < max; k++)
+
+            {
+
+                for (int i = 0; i <= k; i++)
+
+                {
+
+                    String space = "";
+
+                    String star = "";
+
+                    for (int j = 0; j < max - i; j++) space += " ";
+
+                    for (int j = 0; j < 2 * i + 1; j++) star += "*";
+
+                    Console.WriteLine(space + star);
+                }
+            }
+        }
+    }
+}
+        
