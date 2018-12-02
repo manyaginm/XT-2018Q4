988a0bb9ba9fd5eef904fcc24fb89f56f04b2fe7
Mikhail Manyagin <manyaginm@gmail.com>
2018-12-03 00:04:25 +0400

    AverageStringLength

---
+++ Epam.Task1/AVERAGE STRING LENGTH/AVERAGE STRING LENGTH/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AVERAGE_STRING_LENGTH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int bukvcount = 0;
+            int separators = 0;
+            int wordcount = 0;
+            char previous ;
+
+
+            String k = "o.n,e ,t?w.o th#ree&";
+            char[] str = k.ToCharArray();
+            for(int i = 0; i<str.Length; i++)
+            {
+                if (char.IsSeparator(str[i]) | (char.IsPunctuation(str[i]) & i == str.Length - 1))
+                {
+                    wordcount++;
+                }
+                if (char.IsLetter(str[i]))
+                {
+                    bukvcount++;
+                }
+            
+            }
+            double p = (bukvcount * 1.0) / (wordcount*1.0);
+            Console.WriteLine(wordcount * 1.0);
+            Console.WriteLine(bukvcount * 1.0);
+            Console.WriteLine(p);
+        }
+    }
+}
