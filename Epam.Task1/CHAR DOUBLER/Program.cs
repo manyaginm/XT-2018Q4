659a880cd513084aed880b0ee9ddf7b06d08c39b
Mikhail Manyagin <manyaginm@gmail.com>
2018-12-03 00:28:06 +0400

    Char doubler

---
+++ Epam.Task1/CHAR DOUBLER/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+namespace CHAR_DOUBLER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = " написать программу, которая";
+            string two = " описание     ,";
+
+            char[] f = one.ToCharArray();
+            char[] t = two.ToCharArray();
+            StringBuilder res = new StringBuilder();
+            for(int i =0; i < f.Length; i++)
+            {
+                for (int k = 0; k < t.Length; k++)
+                {
+                    if (char.ToLower(f[i]).Equals(char.ToLower(t[k]))
+                        &!(char.IsPunctuation(t[k])|char.IsSeparator(t[k])))
+                    {
+                        res.Append(f[i]);
+
+                    }
+                }
+                res.Append(f[i]);
+            }
+            Console.WriteLine(res.ToString());
+
+        }
+    }
+}
