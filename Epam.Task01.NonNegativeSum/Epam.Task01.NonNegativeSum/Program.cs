be6a9e6c9b3979f17b7304f713fd015f49603984
Mikhail Manyagin <manyaginm@gmail.com>
2018-12-02 23:07:29 +0400

    Epam.Task01.NonNegativeSum

---
+++ Epam.Task01.NonNegativeSum/Epam.Task01.NonNegativeSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Epam.Task01.NonNegativeSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+          
+            Random r = new Random();
+
+
+            Console.WriteLine("Insert array length");
+            int[] mas = new int[int.Parse(Console.ReadLine())];
+            Console.WriteLine("Insert min border");
+            int min = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insert max border");
+            int max = int.Parse(Console.ReadLine());
+            for (int i =0; i< mas.Length; i++)
+            {
+                mas[i] = r.Next(min, max);
+            }
+            Console.WriteLine("Your array: ");
+            foreach(int l in mas)
+            {
+                if (l > 0)
+                {
+                    sum =sum+ l;
+                }
+                Console.WriteLine(l);
+            }
+            Console.WriteLine("The sum is " + sum);
+        }
+    }
+}
