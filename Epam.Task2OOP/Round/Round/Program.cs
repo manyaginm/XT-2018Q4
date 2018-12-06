d012cbbdd96d0e0fd2d51e79d463df40f00c36c2
Mikhail Manyagin <manyaginm@gmail.com>
2018-12-07 00:28:54 +0400

    Task02.Round

---
+++ Epam.Task2OOP/Round/Round/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Round
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Round r = new Round(8, 23);
+            Console.WriteLine(r.getLength());
+            Console.WriteLine(r.getSquare());
+
+        }
+    }
+}
