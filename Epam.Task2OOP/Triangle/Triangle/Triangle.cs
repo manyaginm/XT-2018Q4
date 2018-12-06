9776c6b411087c232096e70c71735a303c6f6975
Mikhail Manyagin <manyaginm@gmail.com>
2018-12-07 00:50:19 +0400

    Triangle

---
+++ Epam.Task2OOP/Triangle/Triangle/Triangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangle
+{
+    class Triangle
+    {
+        private int a;
+        private int b;
+        private int c;
+
+
+        public Triangle() :this(1,1,1){}
+        public Triangle(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public int getPerimeter()
+        {
+            return (a * b) + (b * c) + (a * c);
+        }
+
+        public double getSquare()
+        {
+            int p = ((a * b) + (b * c) + (a * c)) / 2;
+
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
