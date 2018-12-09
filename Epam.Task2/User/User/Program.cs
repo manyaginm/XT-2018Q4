030c18fae397331a02072b3b10917620975d5c78
mmanyagin <manyaginm@gmail.com>
2018-12-09 14:28:29 +0400

    USer

---
+++ Epam.Task2/User/User/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace User
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first name");
+            string fName  = Console.ReadLine();
+            Console.WriteLine("Enter middle name");
+            string mName = Console.ReadLine();
+            Console.WriteLine("Enter last name");
+            string lName = Console.ReadLine();
+            Console.WriteLine("Enter date of birth in format year-month-day");
+            string birthDate = Console.ReadLine();
+
+            User u = new User(fName, lName, mName, birthDate);
+
+            Console.WriteLine(u.getInfo());
+           
+        }
+    }
+}
