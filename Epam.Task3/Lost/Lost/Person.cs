01f86a872b30f15694a9bc19b64df38a93294de5
mmanyagin <manyaginm@gmail.com>
2019-01-02 15:02:48 +0400

    Lost

---
+++ Epam.Task3/Lost/Lost/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Lost
+{
+    class Person
+    {
+        int number;
+
+        public int Number { get => number; set => number = value; }
+
+        public Person(int number)
+        {
+            this.Number = number;
+        }
+
+    }
+}
---
+++ Epam.Task3/Lost/Lost/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 10;
+            Dictionary<int,Person> queue = new Dictionary<int, Person>();
+            for (int i = 1; i <= k; i++)
+            {
+                queue.Add(i,new Person(i));
+            }
+
+            while (queue.Count != 1)
+            {
+                int peopleCount = queue.Count;
+                Dictionary<int, Person> tmpQueue = new Dictionary<int, Person>();
+                int p = 1;
+                for (int i =1; i< peopleCount; i++)
+                {
+                   
+
+                    if (i % 2 == 0)
+                    {
+                        queue.Remove(i);
+                    }
+                    else
+                    {
+                        tmpQueue.Add(p, queue[i]);
+                        p++;
+                    }
+                   
+                }
+                Console.WriteLine("In queue now {0} people", queue.Count);
+                queue = null;
+                queue =  tmpQueue;
+            }
+           
+        }
+    }
+}
+    
