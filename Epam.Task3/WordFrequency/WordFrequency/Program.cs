8e8b51cfc3d146d7792b89ef375e78dfefac02a0
mmanyagin <manyaginm@gmail.com>
2019-01-02 15:37:31 +0400

    WordFrequency

---
+++ Epam.Task3/WordFrequency/WordFrequency/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordFrequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s1 = "Mama mila ramu mama rammu mila";
+          
+            String[] words = s1.ToLower().Split(new char[]{ ' ' , ',','.'});
+
+            Dictionary<String, int> wordDictionary = new Dictionary<string, int>();
+            foreach (String s in words)
+            {
+                if (wordDictionary.ContainsKey(s)) {
+                    wordDictionary[s] = wordDictionary[s] + 1;
+                }
+                else
+                {
+                    wordDictionary.Add(s, 1);
+                }
+            }
+
+          foreach(KeyValuePair<String, int> keyValue in wordDictionary){
+                {
+                    Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
+                }
+            }
+        }
+    }
+}
